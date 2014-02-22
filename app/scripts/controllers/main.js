e4e62e3946cb60cd97770123675a708c175cad2c
Alejandro Inestal <ainestal@gmail.com>
2014-02-22 11:32:49 +0100

    show the volumes info behind the instance info

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,13 +22,20 @@ angular.module('awsCostMonitorApp')
       .success(function(response) {
         $scope.instancesList = response[0];
         $scope.volumesList   = response[1];
+        $scope.totalCost      = 0;
 
-        var index;
-        for (index = 0; index < instancesList.length; ++index) {
-          if (instancesList[index].current_cost != 'N/A. Stopped instance'){
-            $scope.totalCost = $scope.totalCost + instancesList[index].current_cost;
+// solo está insertando en la primera instancia
+        for (var i = 0; i < $scope.instancesList.length; i++){
+          $scope.instancesList[i]['volumes'] = [];
+          for (var j = 0; j < $scope.volumesList.length; j++){
+            if ($scope.instancesList[i]['id'] == $scope.volumesList[j]['instance_id']){              
+              $scope.instancesList[i]['volumes'].push($scope.volumesList[j]);
+            }
           }
-        }
+          if ($scope.instancesList[i]['current_cost'] != 'N/A. Stopped instance'){
+            $scope.totalCost = $scope.totalCost + $scope.instancesList[i]['current_cost'];
+          }
+        };
       })
       .error(console.log('error retrieving the list'));
   });
